e02bef7e69aba8157343c84c5e1f74e5e6f51075
Adriana Ruiz <aruiz@pa-co.com>
2024-05-14 10:56:27 -0500

    Agregar archivos de proyecto.

---
+++ Views/vEstudiante.xaml.cs
@@ -0,0 +1,26 @@
+using aruizT6.Models;
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+
+namespace aruizT6.Views;
+
+public partial class vEstudiante : ContentPage
+{
+	private const string url = "http://192.168.2.89/moviles/wsestudiantes.php";
+	private readonly HttpClient cliente= new HttpClient();
+	private ObservableCollection<Estudiante> est;
+	public vEstudiante()
+	{
+		InitializeComponent();
+		ObtenerDatos();
+
+    }
+
+	public async void ObtenerDatos()
+	{
+		var content = await cliente.GetStringAsync(url);
+		List<Estudiante> mostrar = JsonConvert.DeserializeObject<List<Estudiante>>(content);
+		est= new ObservableCollection<Estudiante>(mostrar);
+		listEstudiante.ItemsSource = est;
+	}
+}
